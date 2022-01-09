63c8984cca890570d253273d9f253fa7643f2d31
Jonathan Rascher <jon@bcat.name>
2022-01-08 22:53:44 -0600

    Recalibrate E-steps for aluminum extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -930,7 +930,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 97.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
