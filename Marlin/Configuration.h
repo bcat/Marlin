2ff85f35f72fd90838fa18629e648705d8236017
Jonathan Rascher <jon@bcat.name>
2022-01-16 17:36:14 -0600

    Calibrate probe XY offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1182,7 +1182,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -9, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -45.3, -3.8, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
